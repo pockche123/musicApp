ca070b52160831dbb5e1288b5fd82ea94e1eddb7
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-12-04 21:01:22 +0000

    change config file

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,8 @@ export default defineConfig({
   server: {
     port: 3000, // Customize the port if needed
     open: true, // Automatically open the browser on server start
+    strictPort: true,
+    host: true
   },
   resolve: {
     alias: {
